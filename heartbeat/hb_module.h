6991d558ce4fed91887c47c88e69e3de01e35fe3
marcelo <devnull@localhost>
2000-09-06 15:54:45 +0000

    Removed declarations of functions only used in module.c
    CVS patchset: 307
    CVS date: 2000/09/06 15:54:45

--- heartbeat/hb_module.h
+++ heartbeat/hb_module.h
@@ -9,11 +9,6 @@ struct symbol_str {
 	int mandatory; 
 };
 
-int so_select (const struct dirent *);
-
-int generic_symbol_load(struct symbol_str[], int len, void **);
 int module_init(void);
-int comm_module_init(void);
 int auth_module_init(void);
-
 
