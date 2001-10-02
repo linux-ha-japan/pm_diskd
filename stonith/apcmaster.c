7b6011cc38a885f90c90c1461dfd5761b005f8f5
alan <devnull@localhost>
2001-10-02 04:43:34 +0000

    Added APCmaster SNMP control mechanism plugin...
    CVS patchset: 795
    CVS date: 2001/10/02 04:43:34

--- stonith/apcmaster.c
+++ stonith/apcmaster.c
@@ -49,8 +49,9 @@
 /*
  * Version string that is filled in by CVS
  */
-static const char *version __attribute__ ((unused)) = "$Revision: 1.7 $"; 
+static const char *version __attribute__ ((unused)) = "$Revision: 1.8 $"; 
 
+#include <portability.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
