66e81837884d20cd32188b4540e0b0db7ab1fd83
msoffen <devnull@localhost>
2002-05-28 18:25:48 +0000

    Changes to replace send_arp with a libnet based version.  This works accross all operating systems we currently "support" (Linux, FreeBSD, Solaris).
    CVS patchset: 1078
    CVS date: 2002/05/28 18:25:48

--- heartbeat/send_arp.c
+++ heartbeat/send_arp.c
@@ -1,203 +1,144 @@
-const static char * _send_arp_c = "$Id: send_arp.c,v 1.6 2001/10/24 00:21:58 alan Exp $";
-/* send_arp.c
+const static char * _send_arp_c = "$Id: send_arp.c,v 1.7 2002/05/28 18:25:48 msoffen Exp $";
+/* 
+send_arp
 
 This program sends out one ARP packet with source/target IP and Ethernet
-hardware addresses suuplied by the user.  It compiles and works on Linux
-and will probably work on any Unix that has SOCK_PACKET.
+hardware addresses suuplied by the user.  It uses the libnet libary from
+Packet Factory (http://www.packetfactory.net/libnet/ ). It has been tested
+on Linux, FreeBSD, and on Solaris.
 
-The idea behind this program is a proof of a concept, nothing more.  It
-comes as is, no warranty.  However, you're allowed to use it under one
-condition: you must use your brain simultaneously.  If this condition is
-not met, you shall forget about this program and go RTFM immediately.
+This is based on the sample application supplied by Packet Factory.
 
-yuri volobuev'97
-volobuev@t1.chem.umn.edu
+Matt Soffen
 
 */
 
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#if 0
-#	include <linux/in.h>
-#endif
-#include <netinet/in.h>
-#include <net/if.h>
-#include <netinet/if_ether.h>
-#include <arpa/inet.h>
+#include <libnet.h>
 
-#ifdef linux
-#	define	NEWSOCKET()	socket(AF_INET, SOCK_PACKET, htons(ETH_P_RARP))
-#else
-#	define	NEWSOCKET()	socket(SOL_SOCKET, SOCK_RAW, ETHERTYPE_REVARP)
-#endif
+int send_arp(struct libnet_link_int *l, u_long ip, u_char *device, u_char *macaddr, u_char *broadcast, u_char *netmask);
 
-#define ETH_HW_ADDR_LEN 6
-#define IP_ADDR_LEN 4
-#define ARP_FRAME_TYPE 0x0806
-#define ETHER_HW_TYPE 1
-#define IP_PROTO_TYPE 0x0800
-#define OP_ARP_REQUEST	1
-#define OP_ARP_REPLY	2
-
-
-char usage[]={"send_arp: sends out custom ARP packet. yuri volobuev'97\n\
+char print_usage[]={"send_arp: sends out custom ARP packet. packetfactory.net\n\
 \tusage: send_arp dev src_ip_addr src_hw_addr targ_ip_addr tar_hw_addr\n\n"};
 
-struct arp_packet {
-        u_char targ_hw_addr[ETH_HW_ADDR_LEN];
-        u_char src_hw_addr[ETH_HW_ADDR_LEN];
-        u_short frame_type;
-        u_short hw_type;
-        u_short prot_type;
-        u_char hw_addr_size;
-        u_char prot_addr_size;
-        u_short op;
-        u_char sndr_hw_addr[ETH_HW_ADDR_LEN];
-        u_char sndr_ip_addr[IP_ADDR_LEN];
-        u_char rcpt_hw_addr[ETH_HW_ADDR_LEN];
-        u_char rcpt_ip_addr[IP_ADDR_LEN];
-        u_char padding[18];
-};
-
-void die(const char *);
-void get_ip_addr(struct in_addr*,char*);
-void get_hw_addr(u_char*,char*);
-
-#define	DIMOF(a)	(sizeof(a)/sizeof(a[0]))
+void convert_macaddr (u_char *macaddr, u_char enet_src[6]);
 
 int
-main(int argc,char** argv)
+main(int argc, char *argv[])
 {
+    int c;
+    char errbuf[256];
+    char *device = NULL;
+    char *macaddr = NULL;
+    char *broadcast = NULL;
+    char *netmask = NULL;
+    struct libnet_link_int *l;
+    u_long ip;
 
-	struct in_addr src_in_addr,targ_in_addr;
-	struct arp_packet pkt;
-	struct sockaddr sa;
-	int	sock;
-	int	optypes [] = { OP_ARP_REQUEST, OP_ARP_REPLY};
-	int	j;
+    if (argc != 6) {
+        printf("%s", print_usage);
+	exit (-1);
+    }
 
+    /*
+    argv[1] DEVICE	dc0,eth0:0,hme0:0,
+    argv[2] IP		192.168.195.186
+    argv[3] MAC ADDR	00a0cc34a878
+    argv[4] BROADCAST	192.168.195.186
+    argv[5] NETMASK	ffffffffffff
+    */
 
-	(void)_send_arp_c;
+    device = argv[1];
+    macaddr = argv[3];
+    broadcast = argv[4];
+    netmask = argv[5];
+    
+    if ((ip = libnet_name_resolve(argv[2], 1)) == -1)
+    {
+        fprintf(stderr, "Cannot resolve IP address\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if (argc != 6) {
-		die(usage);
-	}
+    l = libnet_open_link_interface(device, errbuf);
+    if (!l)
+    {
+        fprintf(stderr, "libnet_open_link_interface: %s\n", errbuf);
+        exit(EXIT_FAILURE);
+    }
 
-	sock=NEWSOCKET();
-	if (sock < 0) {
-		perror("socket");
-		exit(1);
-        }
-
-	/* Most switches/routers respond to the ARP reply, a few only
-	 * to an ARP request.  RFCs say they should respond
-	 * to either.  Oh well... We'll try and work with all...
-	 * So, we broadcast both an ARP request and a reply...
-	 * See RFCs 2002 and 826.
-	 *
-	 * The observation about some only responding to ARP requests
-	 * came from Masaki Hasegawa <masaki-h@pp.iij4u.or.jp>.
-	 * So, this fix is due largely to him.
-	 */
-
-	for (j=0; j < DIMOF(optypes); ++j) {
-		pkt.frame_type = htons(ARP_FRAME_TYPE);
-		pkt.hw_type = htons(ETHER_HW_TYPE);
-		pkt.prot_type = htons(IP_PROTO_TYPE);
-		pkt.hw_addr_size = ETH_HW_ADDR_LEN;
-		pkt.prot_addr_size = IP_ADDR_LEN;
-		pkt.op=htons(optypes[j]);
-
-		get_hw_addr(pkt.targ_hw_addr,argv[5]);
-		get_hw_addr(pkt.rcpt_hw_addr,argv[5]);
-		get_hw_addr(pkt.src_hw_addr,argv[3]);
-		get_hw_addr(pkt.sndr_hw_addr,argv[3]);
-
-		get_ip_addr(&src_in_addr,argv[2]);
-		get_ip_addr(&targ_in_addr,argv[4]);
-
-		memcpy(pkt.sndr_ip_addr,&src_in_addr,IP_ADDR_LEN);
-		memcpy(pkt.rcpt_ip_addr,&targ_in_addr,IP_ADDR_LEN);
-
-		memset(pkt.padding,0, 18);
-
-		strcpy(sa.sa_data,argv[1]);
-		if (sendto(sock, (const void *)&pkt,sizeof(pkt),0
-		,	&sa,sizeof(sa)) < 0) {
-			perror("sendto");
-			exit(1);
-		}
-	}
-	exit(0);
+    c = send_arp(l, ip, device, macaddr, broadcast, netmask);
+    printf("\n");
+    return (c == -1 ? EXIT_FAILURE : EXIT_SUCCESS);
 }
 
-void die(const char* str)
-{
-	fprintf(stderr,"%s\n",str);
-	exit(1);
-}
 
-void get_ip_addr(struct in_addr* in_addr,char* str)
+void convert_macaddr (u_char *macaddr, u_char enet_src[6])
 {
-	struct hostent *hostp;
+	int i, pos;
+	u_char bits[3];
 
-	in_addr->s_addr=inet_addr(str);
-	if (in_addr->s_addr == -1) {
-		if ( (hostp = gethostbyname(str))) {
-			memcpy(in_addr, hostp->h_addr, hostp->h_length);
-		}else{
-			fprintf(stderr,"send_arp: unknown host %s\n",str);
-			exit(1);
-		}
+	pos = 0;
+	for (i = 0; i < 6; i++)
+	{
+	    bits[0] = macaddr[pos++];
+	    bits[1] = macaddr[pos++];
+	    bits[2] = '\0';
+
+	    enet_src[i] = strtol(bits, (char **)NULL, 16);
 	}
+
 }
 
-void get_hw_addr(u_char* buf,char* str)
+int
+send_arp(struct libnet_link_int *l, u_long ip, u_char *device, u_char *macaddr, u_char *broadcast, u_char *netmask)
 {
+    int n;
+    u_char *buf;
+    u_char enet_src[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+    u_char enet_dst[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
-	int i;
-	char c,val = 0;
 
-	for(i=0;i<ETH_HW_ADDR_LEN;i++) {
-		if ( !(c = tolower((unsigned int)(*str++)))) {
-			die("Invalid hardware address");
-		}
-		if (isdigit((unsigned int)c)) {
-			val = c-'0';
-		}else if (c >= 'a' && c <= 'f') {
-			val = c-'a'+10;
-		}else{
-			die("Invalid hardware address");
-		}
+    if (libnet_init_packet(LIBNET_ARP_H + LIBNET_ETH_H, &buf) == -1)
+    {
+        perror("libnet_init_packet memory:");
+        exit(EXIT_FAILURE);
+    }
 
-		*buf = val << 4;
+    /* Convert ASCII Mac Address to 6 Hex Digits. */
+    convert_macaddr (macaddr, enet_src);
 
-		if ( !(c = tolower(*str++))) {
-			die("Invalid hardware address");
-		}
+    /* Ethernet header */
+    libnet_build_ethernet(enet_dst, enet_src, ETHERTYPE_ARP, NULL, 0, buf);
 
-		if (isdigit((unsigned int)c)) {
-			val = c-'0';
-		}else if (c >= 'a' && c <= 'f') {
-			val = c-'a'+10;
-		}else{
-			die("Invalid hardware address");
-		}
+    /*
+     *  ARP header
+     */
+    libnet_build_arp(ARPHRD_ETHER,
+        ETHERTYPE_IP,
+        6,
+        4,
+        ARPOP_REQUEST,
+        enet_src,
+        (u_char *)&ip,
+        enet_dst,
+        (u_char *)&ip,
+        NULL,
+        0,
+        buf + LIBNET_ETH_H);
 
-		*buf++ |= val;
+    n = libnet_write_link_layer(l, device, buf, LIBNET_ARP_H + LIBNET_ETH_H);
 
-		if (*str == ':')str++;
-        }
+    fprintf(stderr, ".");
+
+    libnet_destroy_packet(&buf);
+    return (n);
 }
 
 /*
  * $Log: send_arp.c,v $
+ * Revision 1.7  2002/05/28 18:25:48  msoffen
+ * Changes to replace send_arp with a libnet based version.  This works accross
+ * all operating systems we currently "support" (Linux, FreeBSD, Solaris).
+ *
  * Revision 1.6  2001/10/24 00:21:58  alan
  * Fix to send both a broadcast ARP request as well as the ARP response we
  * were already sending.  All the interesting research work for this fix was
