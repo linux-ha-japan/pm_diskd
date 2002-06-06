e6359c8b586192659b95744afc747d4240ca3adf
alan <devnull@localhost>
2002-06-06 06:35:08 +0000

    Added license statement, etc.
    CVS patchset: 1097
    CVS date: 2002/06/06 06:35:08

--- lib/apphb/apphb.c
+++ lib/apphb/apphb.c
@@ -1,3 +1,25 @@
+/*
+ * apphb.c: application heartbeat library code.
+ *
+ * Copyright (C) 2002 Alan Robertson <alanr@unix.sh>
+ *
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 #include <portability.h>
 #include <string.h>
 #include <unistd.h>
@@ -19,7 +41,7 @@ static int			hbstatus = -1;
 
 static int apphb_getrc(void);
 
-/* Get return code from previous operation */
+/* Get return code from last operation */
 static int
 apphb_getrc(void)
 {
