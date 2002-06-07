0125d7152291fa5f4a7206ab611b8ef98791da74
alan <devnull@localhost>
2002-06-07 06:02:25 +0000

    Added apphbtest.c to CVS and apphbtest to the .cvsignore file.
    CVS patchset: 1103
    CVS date: 2002/06/07 06:02:25

---
+++ telecom/apphbd/apphbtest.c
@@ -0,0 +1,80 @@
+/*
+ * apphbtest:	application heartbeat test program
+ *
+ * This program registers with the application heartbeat server
+ * and just issues heartbeats from time to time...
+ *
+ * Copyright(c) 2002 Alan Robertson <alanr@unix.sh>
+ *
+ *********************************************************************
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <portability.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <apphb.h>
+
+int
+main(int argc,char ** argv)
+{
+	int	j;
+	int	rc;
+
+	fprintf(stderr, "Client starting - pid: %d\n", getpid());
+	rc = apphb_register("test program");
+	if (rc < 0) {
+		perror("registration failure");
+		sleep(5);
+		exit(1);
+	}
+	
+	fprintf(stderr, "Client setting 2 second heartbeat period\n");
+	apphb_setinterval(2000);
+	if (rc < 0) {
+		perror("setinterval failure");
+		exit(2);
+	}
+
+	for (j=0; j < 10; ++j) {
+		sleep(1);
+		fprintf(stderr, "+");
+		rc = apphb_hb();
+		if (rc < 0) {
+			perror("apphb_hb failure");
+			exit(3);
+		}
+
+	}
+	fprintf(stderr, "\n");
+	sleep(3);
+	fprintf(stderr, "!");
+	rc = apphb_hb();
+	if (rc < 0) {
+		perror("late apphb_hb failure");
+		exit(4);
+	}
+
+	fprintf(stderr, "Client unregistering\n");
+	rc = apphb_unregister();
+	if (rc < 0) {
+		perror("apphb_unregister failure");
+		exit(5);
+	}
+	return(0);
+}
