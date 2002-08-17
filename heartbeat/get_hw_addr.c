a71f79b9b16826fbc4d8b54b8bf3420c863b37e1
alan <devnull@localhost>
2002-08-17 06:55:31 +0000

    Fixed a couple of compile errors. libnet prototype for its error function is incorrect (worked around) Libnet library doesn't include prototype for ether_ntoa().
    CVS patchset: 1246
    CVS date: 2002/08/17 06:55:31

--- heartbeat/get_hw_addr.c
+++ heartbeat/get_hw_addr.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <syslog.h>
 
+const char * ether_ntoa(struct ether_addr *macaddr);
+
 int
 main(int argc, char *argv[])
 {
@@ -13,6 +15,11 @@ main(int argc, char *argv[])
 	struct libnet_link_int	*network;
 	u_char			*device;
 	char			err_buf[LIBNET_ERRBUF_SIZE];
+
+	/* Get around bad prototype for libnet_error() */
+
+	char errmess1 [] = "libnet_open_link_interface: %s\n";
+	char errmess2 [] = "libnet_get_hwaddr: %s\n";
 
 	if (argc != 2) {
 		syslog(LOG_ERR,"usage: get_hw_addr <INTERFACE-NAME>\n");
@@ -22,11 +29,11 @@ main(int argc, char *argv[])
 	device = argv[1];
 
 	if ( (network = libnet_open_link_interface(device, err_buf)) == NULL) {
-		libnet_error(LIBNET_ERR_FATAL, "libnet_open_link_interface: %s\n", err_buf);
+		libnet_error(LIBNET_ERR_FATAL, errmess1, err_buf);
 	}
 
        if ( (mac_address = libnet_get_hwaddr(network, device, err_buf)) == 0) {
-		libnet_error(LIBNET_ERR_FATAL, "libnet_get_hwaddr: %s\n", err_buf);
+		libnet_error(LIBNET_ERR_FATAL, errmess2, err_buf);
 	}
 
 	printf("%s", ether_ntoa(mac_address));
