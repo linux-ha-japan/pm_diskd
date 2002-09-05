2bb91b76dfefc371fe3ac7f94ae49451216bff56
alan <devnull@localhost>
2002-09-05 06:29:09 +0000

    Converted get_hw_addr.c to use the newer libnet API if present...
    CVS patchset: 1285
    CVS date: 2002/09/05 06:29:09

--- heartbeat/get_hw_addr.c
+++ heartbeat/get_hw_addr.c
@@ -8,6 +8,8 @@
 
 const char * ether_ntoa(struct ether_addr *macaddr);
 
+#ifdef HAVE_LIBNET_1_0_API
+
 int
 main(int argc, char *argv[])
 {
@@ -36,7 +38,43 @@ main(int argc, char *argv[])
 		libnet_error(LIBNET_ERR_FATAL, errmess2, err_buf);
 	}
 
-	printf("%s", ether_ntoa(mac_address));
+	printf("%s\n", ether_ntoa(mac_address));
 
 	exit(0);
 }
+#endif
+
+#ifdef HAVE_LIBNET_1_1_API
+int
+main(int argc, char *argv[])
+{
+	struct libnet_ether_addr	*mac_address;
+	libnet_t		*ln;
+	u_char			*device;
+	char			err_buf[LIBNET_ERRBUF_SIZE];
+
+	/* Get around bad prototype for libnet_error() */
+
+
+	if (argc != 2) {
+		syslog(LOG_ERR,"usage: get_hw_addr <INTERFACE-NAME>\n");
+		exit(1);
+	}
+
+	device = argv[1];
+
+	if ( (ln = libnet_init(LIBNET_LINK, device, err_buf)) == NULL) {
+		fprintf(stderr, "libnet_open_link_interface: %s\n", err_buf);
+		exit(1);
+	}
+
+       if ( (mac_address = libnet_get_hwaddr(ln)) == 0) {
+		fprintf(stderr,  "libnet_get_hwaddr: %s\n", err_buf);
+		exit(1);
+	}
+
+	printf("%s\n", ether_ntoa((struct ether_addr*)mac_address));
+
+	exit(0);
+}
+#endif
