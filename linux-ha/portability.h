5581c42a0d88e6e87e4075f28775cd8c7e8783ae
alan <devnull@localhost>
2003-05-20 05:55:10 +0000

    Got rid of the CLK_TCK stuff from configure.in and related places...
    CVS patchset: 1919
    CVS date: 2003/05/20 05:55:10

--- linux-ha/portability.h
+++ linux-ha/portability.h
@@ -99,34 +99,6 @@ inet_pton(int af, const char *src, void *dst);
 
 #endif /* HA_HAVE_INET_PTON */
 
-/*
- * Special Note:  CLK_TCK is *not* the same as CLOCKS_PER_SEC.
- *
- * CLOCKS_PER_SEC is supposed to be 1000000 on every system.
- * This is most certainly *not* what we need.
- *
- * Older UNIX systems used to call this (CLK_TCK) HZ.
- *
- * NOTE:  We're moving away from times - to the longclock_t types
- * which solve this problem much more nicely...
- *
- */
-#ifdef HA_CLK_TCK_IN_TIME_H
-#  include <time.h>
-#else
-#  ifdef HA_CLK_TCK_IN_LIMITS_H
-#    include <limits.h>
-#  endif
-#endif
-#ifndef CLK_TCK
-#  include <unistd.h>
-#  ifdef _SC_CLK_TCK
-#    define CLK_TCK	((clock_t)sysconf(_SC_CLK_TCK))
-#  else
-#    error "No definition for CLK_TCK available"
-#  endif
-#endif
-
 #ifndef HA_HAVE_STRNLEN
 #	define	strnlen(a,b) strlen(a)
 #else
